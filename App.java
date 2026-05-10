4520b4472685c476ed3aab457cbeca624b168cd0
Sreethu S <srs23csaiml@cmrit.ac.in>
2026-05-10 21:14:55 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.prog4.maven.PROG6_out_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
